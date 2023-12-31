30e8ea7086fc84d4cd096ba03c3bdd50a2a3377b
lolito <prof.lolito@gmail.com>
2023-12-31 12:31:52 +0200

    react-icons added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import logo from './../assets/Images/logo.png';
+import { IoLogoYoutube } from "react-icons/io5";
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='flex justify-between items-center'>
+      <img src={logo} alt="logo" className='w-[180px]'/>
+      <ul className='flex gap-4 md:gap-14'>
+        <li className='hover:font-bold cursor-pointer'>Home</li>
+        <li className='hover:font-bold cursor-pointer'>About radblok</li>
+        <li className='hover:font-bold cursor-pointer'>Contact Me</li>
+      </ul>
+      <button className='bg-red-500 rounded-full text-white'>
+        Subscribe <IoLogoYoutube />
+      </button>
+    </div>
   )
 }
 
